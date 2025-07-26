a98509bd655eb355dd8a520b77c8f3a9daf8d1d5
PrantoMondol11 <mondolpranto83@gmail.com>
2025-07-26 13:12:25 -0500

    My Calculator App

--- Calculator_With_History.py
+++ Calculator_With_History.py
@@ -1,10 +1,38 @@
-f = open("History.txt", "a")
 print("Welcome To My Calculator App!")
 print("............................")
-print("Enter Your Equation Here:")
-eq=str(input())
-res=str(eval(eq))
 
-his=eq + "=" + res
-print(his)
-f.write(his)
+while (True):
+    f = open("History.txt", "a")
+    print("\n1.Calculate\n2.History\n3.Clear\n4.Exit")
+    
+    opt=int(input())
+    
+
+
+    if opt==1:
+        print("Enter Your Equation Here:")
+        eq=str(input())
+        res=str(eval(eq))  
+        his=eq + "=" + res
+        print("Result:" + his)
+        f.write(his+"\n")  
+    elif opt==2:
+        f = open("History.txt", "r") 
+        content=f.read()
+        if content == "":
+            print("Nothing to show!")
+        else:
+            print("History:")
+            print("..........................")
+            print(content)
+    elif opt==3:
+        f.seek(0)
+        f.truncate()
+        print("Cleared")
+    elif opt==4:
+        break
+    else:
+        print("Invalid Option!")
+
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+print(eq+"="+res)
