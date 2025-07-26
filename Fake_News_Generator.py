fa577515c115b5446c900029a6ffa706d4c4d572
PrantoMondol11 <mondolpranto83@gmail.com>
2025-07-26 11:26:57 -0500

    Tried somthing on own

---
+++ Fake_News_Generator.py
@@ -0,0 +1,19 @@
+import random as rn
+
+subjects=["Sarjis","Sakib","Tamim","Pranto"]
+actions=["eat","killed","play","murdered"]
+objects=["table","Buildings","football","mobile","at school"]
+opt="yes"
+
+while(opt=="yes"):
+    new1=rn.choice(subjects)
+    new2=rn.choice(actions)
+    new3=rn.choice(objects)
+    
+    print(".....................................")
+    print( "-: " + new1 +" "+ new2 +" "+new3+"." )
+    print(".....................................")
+
+    print("Do you want to generate another one:")
+    opt=str(input())
+    
