58044208183b68f6aba06dbe0913eb496141e83d
PrantoMondol11 <mondolpranto83@gmail.com>
2025-07-26 13:40:37 -0500

    Fixed some bug

--- Calculator_With_History.py
+++ Calculator_With_History.py
@@ -2,7 +2,6 @@ print("Welcome To My Calculator App!")
 print("............................")
 
 while (True):
-    f = open("History.txt", "a")
     print("\n1.Calculate\n2.History\n3.Clear\n4.Exit")
     
     opt=int(input())
@@ -15,11 +14,13 @@ while (True):
         res=str(eval(eq))  
         his=eq + "=" + res
         print("Result:" + his)
-        f.write(his+"\n")  
+        f = open("History.txt", "a")
+        f.write(his+"\n") 
+        f.close() 
     elif opt==2:
         f = open("History.txt", "r") 
         lines = f.readlines()
-        if lines == "":
+        if not lines:
             print("Nothing to show!")
         else:
             lines=reversed(lines)
@@ -28,10 +29,13 @@ while (True):
             for line in lines:
                 
                 print(line,end="")
+        f.close()
     elif opt==3:
+        f=open("History.txt","a")
         f.seek(0)
         f.truncate()
         print("Cleared")
+        f.close()
     elif opt==4:
         break
     else:
